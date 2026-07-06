518257efe09ee2eb35a20eb2a7b492ad5f786dbd
Stephen <gamer03sd@gmail.com>
2026-07-06 11:11:54 -0500

    Add code & update Readme

--- Pictures/SENPROTO/SENPROTO.ino
+++ Pictures/SENPROTO/SENPROTO.ino
@@ -1,61 +1,4 @@
-
-
-
-# Senior Design Project
-
-## **Laser Imaging Detection and Ranging <br> (LiDAR) Based Proximity Alert <br> for Bicycle Helmet**
-
-### Part 1: Planning
-
-An important part of any project is the planning stage, this project is no exception. <br> Below is the table in the report to show when each part of the project was completed.
-![](https://github.com/VancouverSatelite/Senior-Design/blob/main/Pictures/schedule.png)
-<br>
-Now, you may notice some missing lines, try to ignore that.
-
-This project was done as a group, but I did the 3D modeling, the Wiring Diagram, and general part oversight.
-<br> I was the main developer, however my groupmates helped me a lot with testing, and with putting together presentation materials.
-
----
-
-### Part 2: Initial Design
-
-Keeping to schedule is a key part of project productivity, however just scheduling isn't going ot do all of the work.
-
-The first step of designing this project was to understand what exactly we were supposed to be making.
-
-When I showed up to class and was told, <br>"You're doing the bicycle helmet thing!" <br> It wasn't super helpful, so I sought out help from my professor, asking him to clarify what exactly it was that our group was supposed to be making.
-
-It turned out to be **"A bicycle helmet mounted device that improved cyclist safety."**
-
-So naturally I was immediately trying to brainstorm how I would go about this.
-
-A common way automobiles go about keeping distance from others is by using a parallel camera system (as depicted below), so maybe this would work, but you know what I've always wanted to work with? LiDAR, which is close enough to that
-![](https://github.com/VancouverSatelite/Senior-Design/blob/main/Pictures/volvo.jpg) Source: [Volvo UK](https://www.volvocars.com/uk/support/car/xc40/article/af42dd367f7db52ec0a80151108cf1cd/)
-<br>
-So I got on that immediately, and started looking around, ending up on the TF-MINIs LiDAR sensor, as it works with arduinos.
-<br>
-Ah yes, Arduinos, the classic low-power low level microcontrollers that are super versatile. Which one should we use?
- 
- Originally, the plan was to use an Arduino Nano, as it's the smallest package you can get that amount of capability in, but unfortunately while trying to get the nano off of a breadboard, I completely biffed it and knocked a resistor off the bottom of it.... Whoops.
-
- This led to us using an Arduino Mega 2650 (from Elegoo) for the majority of the time we were developing the actual device.
-
- ---
-
-### Part 3: Testing 
-
-### Part X: the final Design
-![The Wiring diagram of the project, which I threw together in KICAD](https://github.com/VancouverSatelite/Senior-Design/blob/main/Pictures/FinalDesign.png)
-  [Rear Casing](https://sketchfab.com/3d-models/rear-casing-311979b84c3f495db25b09bcab5221ab)  <br>
-  ![A spinning 3D model which is the design of the rear case portion of the device.](https://github.com/VancouverSatelite/Senior-Design/blob/main/Pictures/trim-rear.gif)<br>
-
-  [Front Casing](https://sketchfab.com/3d-models/front-panel-c0035471094749f2b10e251a77223106)<br>
-  
-  ![A spinning 3D model which is the design of the front case portion of the device.](https://github.com/VancouverSatelite/Senior-Design/blob/main/Pictures/trimmed-front.gif)
-
-  If you are interested in the code used, it was a permutation of this 
-  ```
-  #include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel.h>
 #include <SoftwareSerial.h>
 
 // ---------------- CONFIG ----------------
@@ -228,4 +171,3 @@ void loop() {
     Serial.println(d);
   }
 }
-```
